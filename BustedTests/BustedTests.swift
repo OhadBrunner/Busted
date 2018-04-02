0bc104b0a4522a513ff3310e7b35be4dfbddf272
Ohad Brunner <ohadbrunner2@walla.com>
2018-04-02 17:31:21 +0300

    Initial Commit

---
+++ BustedTests/BustedTests.swift
@@ -0,0 +1,36 @@
+//
+//  BustedTests.swift
+//  BustedTests
+//
+//  Created by Ohad Brunner on 02/04/2018.
+//  Copyright © 2018 Ohad Brunner. All rights reserved.
+//
+
+import XCTest
+@testable import Busted
+
+class BustedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
